5d7553064e79b603e143de869995b22c130e8595
DrVenter <ventercolin1@gmail.com>
2022-06-28 17:08:01 +0200

    finished writing the script to connvert data to csv

--- convert_data_file_to_csv.py
+++ convert_data_file_to_csv.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 class Data_to_CSV:
     experimental_conditions = []
-    
+    bins = []
+    matrix = []
+
     def __init__(self, file_name, extension):
         self.file_name = file_name
         self.extension = extension
 
         file = open(self.file_name + self.extension)
-        self.matrix = []
 
         row_delimiter = "\n"
         column_delimiter = "\t"
@@ -28,67 +29,78 @@ class Data_to_CSV:
             self.matrix[row, label_column] = self.matrix[row, label_column][0:-length_of_text_to_remove]
         self.matrix[0,0] = "Label" #not quite sure why this gets removed
 
+    def save_matrix_to_csv(self):
+        data = pd.DataFrame(self.matrix)
+
+        data.columns = data.iloc[0]
+        data = data[1:]
+
+        for row in data.index:
+            if data.loc[row, "Label"] not in self.experimental_conditions:
+                data.drop(row, inplace = True)
+
+        data.to_csv(f"{file_name}.csv", index=False)
+
+    def create_bin_intervals(self, start, end, number_of_bins):
+        bin_intervals = []
+        interval = (end - start)/number_of_bins
+
+        for bin in range(number_of_bins):
+            bin_intervals.append([round(start, 3), round(start + interval, 3)])
+            start+=interval
+
+        return bin_intervals
+
+    def determine_frequency_distribution(self, label, attribute, number_of_bins, bins):
+        header_row = 0
+        column = np.where(self.matrix[header_row] == attribute)[0]
+        label = np.where(self.matrix[: , 0] == label)
+        x_axis = np.zeros((number_of_bins, 1), dtype=float)
+
+        inner_element = 0
+        lower_limit = 0
+        upper_limit = 1
+        for row in label[inner_element]: 
+            for bin in range(len(bins)):
+                if bins[bin][lower_limit] <= float(self.matrix[row, column][inner_element]) < bins[bin][upper_limit]:
+                    x_axis[bin]+=1
+
+        count = len(label[inner_element])
+        for frequency_count in range(len(x_axis)): x_axis[frequency_count] = x_axis[frequency_count]/count * 100
+        x_axis_percentage = np.around(x_axis, 2)
+
+        return x_axis_percentage
+
+    def create_frequency_distribution_matrix(self, attribute, start, end, number_of_bins):
+        bins = self.create_bin_intervals(start, end, number_of_bins)
+
+        x_axis_label = []
+        lower_limit = 0
+        upper_limit = 1
+        for bin in range(len(bins)):
+            lower_limit_label = str(bins[bin][lower_limit])
+            upper_limit_label = str(bins[bin][upper_limit])
+            x_axis_label.append([lower_limit_label + " - " + "<" + upper_limit_label])
+        x_axis_label = np.array(x_axis_label)
+
+        frequency_matrix = x_axis_label.copy()
+        for label in self.experimental_conditions:
+            temp = self.determine_frequency_distribution(label, attribute, number_of_bins, bins)
+            frequency_matrix = np.concatenate((frequency_matrix, temp), axis=1)
+
+        legend = ["Bins"] + self.experimental_conditions
+        frequency_matrix = np.concatenate((np.array([legend]), frequency_matrix), axis=0)
+
+        frequency_matrix = pd.DataFrame(frequency_matrix)
+        frequency_matrix.columns = frequency_matrix.iloc[0]
+        frequency_matrix = frequency_matrix[1:]
+        frequency_matrix.to_csv(f"{file_name}_frequency_distribution_{attribute}.csv", index=False)
+
 file_name = "fibroblasts_cultured_in_serum_n=1"
 extension = ".xls"
 data = Data_to_CSV(file_name, extension)
-
 data.experimental_conditions = ['(1) LMTK 0%', '(2) LMTK 1%', '(3) LMTK 2%', '(4) LMTK 5%', '(5) LMTK 10%']
+data.save_matrix_to_csv()
 
-def distribution(start, end, number_of_bins):
-    distribution = []
-    interval = (end - start)/number_of_bins
-
-    for x in range(number_of_bins):
-        distribution.append([round(start, 3), round(start + interval, 3)])
-        start+=interval
-
-    return distribution
-
-bins = distribution(0, 1, 10)
-
-def frequency_distribution(label, characteristic, bins):
-    col = np.where(matrix[0] == characteristic)[0]
-    label = np.where(matrix[: , 0] == label)
-    x_axis = np.zeros((len(bins), 1), dtype=float)
-
-    for x in label[0]: 
-        for y in range(len(bins)):
-            if bins[y][0] <= float(matrix[x, col][0]) < bins[y][1]:
-                x_axis[y]+=1
-
-    count = len(label[0])
-    for x in range(len(x_axis)): x_axis[x] = x_axis[x]/count * 100
-    x_axis = np.around(x_axis, 2)
-    #x_axis = x_axis.astype(str)
-
-    return x_axis
-
-#print(frequency_distribution(experimental_conditions[2], "Circ.", bins))
-
-x_axis_label = []
-for x in range(len(bins)):
-    x_axis_label.append([str(bins[x][0]) + " - " + "<" + str(bins[x][1])])
-x_axis_label = np.array(x_axis_label)
-
-frequency_matrix = x_axis_label.copy()
-for x in experimental_conditions:
-    temp = frequency_distribution(x, "Circ.", bins)
-    frequency_matrix = np.concatenate((frequency_matrix, temp), axis=1)
-
-#print(frequency_matrix)
-
-legend = ["bins"] + experimental_conditions
-frequency_matrix = np.concatenate((np.array([legend]), frequency_matrix), axis=0)
-print(frequency_matrix)
-
-
-data = pd.DataFrame(matrix)
-data.columns = data.iloc[0]
-data = data[1:]
-
-for x in data.index:
-    if data.loc[x, "Label"] not in experimental_conditions:
-        data.drop(x, inplace = True)
-
-#data.to_csv(f"{file_name}.csv", index=False)
-
+data.create_frequency_distribution_matrix(attribute="Circ.", start=0, end=1, number_of_bins=10)
+data.create_frequency_distribution_matrix(attribute="Area", start=100, end=200, number_of_bins=20)
